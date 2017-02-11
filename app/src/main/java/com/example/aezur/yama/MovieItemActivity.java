c95176971ef02daf75e35b7b7f1d7c83687ad10b
Peter Mulligan <Peter Mulligan>
2017-02-11 18:08:37 +0000

    Project Upload for storage

---
+++ app/src/main/java/com/example/aezur/yama/MovieItemActivity.java
@@ -0,0 +1,62 @@
+/*
+ * Created on 02/02/17 18:04 by aezur.
+ * Copyright (c) 2017
+ *
+ * Last modified: 08/02/17 11:06
+ */
+
+package com.example.aezur.yama;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.widget.ImageView;
+import android.widget.TextView;
+import com.example.aezur.yama.Utils.JsonUtils;
+import com.example.aezur.yama.Utils.NetworkUtils;
+import com.squareup.picasso.Picasso;
+
+/**
+ * Loads and displays movie data from passed in intent EXTRAs
+ */
+public class MovieItemActivity extends AppCompatActivity {
+    private final static String TAG = MovieItemActivity.class.getSimpleName();
+
+    private TextView mTitleTextView;
+    private TextView mDescriptionTextView;
+    private TextView mReleaseDateTextView;
+    private TextView mRatingTextView;
+    private ImageView mMovieImageView;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_movie_item);
+
+        mTitleTextView = (TextView) findViewById(R.id.tv_movieItem_movieTitle);
+        mDescriptionTextView = (TextView) findViewById(R.id.tv_movieItem_movieDescription);
+        mReleaseDateTextView = (TextView) findViewById(R.id.tv_movieItem_movieReleaseDate);
+        mRatingTextView = (TextView) findViewById(R.id.tv__rating);
+        mMovieImageView = (ImageView) findViewById(R.id.iv_movieItem_movieImage);
+
+        Intent intent = getIntent();
+
+        if(intent.hasExtra("MovieData") && intent.hasExtra("MovieIndex")) {
+            int index = intent.getIntExtra("MovieIndex", 0);
+            String movieData = intent.getStringExtra("MovieData");
+            String movieTitle = JsonUtils.getMovieData(movieData, "title", index);
+            String movieImagePath = JsonUtils.getMovieData(movieData, "poster_path", index);
+            String movieDesc = JsonUtils.getMovieData(movieData, "overview", index);
+            String movieReleaseDate = JsonUtils.getMovieData(movieData, "release_date", index);
+            String rating = JsonUtils.getMovieData(movieData, "vote_average", index);
+            movieImagePath = NetworkUtils.buildPosterUrl(movieImagePath);
+            Picasso.with(getApplicationContext()).load(movieImagePath).into(mMovieImageView);
+            mRatingTextView.setText(rating);
+            mTitleTextView.setText(movieTitle);
+            mDescriptionTextView.setText(movieDesc);
+            mReleaseDateTextView.setText("Release Date: " + movieReleaseDate);
+        }
+    }
+}
---
+++ app/src/main/java/com/example/aezur/yama/Utils/MovieAdapter.java
@@ -0,0 +1,114 @@
+/*
+ * Created on 27/01/17 15:13 by Peter Mulligan.
+ * Copyright (c) 2017
+ *
+ * Last modified: 08/02/17 11:00
+ */
+
+package com.example.aezur.yama.Utils;
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.view.View.OnClickListener;
+
+import com.example.aezur.yama.R;
+import com.squareup.picasso.Picasso;
+
+/**
+ * MovieAdapter handles recycling and caching of the RecyclerView items
+ */
+public class MovieAdapter extends RecyclerView.Adapter<MovieAdapter.MovieViewHolder> {
+    private final static String TAG = MovieAdapter.class.getSimpleName();
+
+    private final OnItemClickHandler mOnClickHandler;
+
+    public interface OnItemClickHandler {
+        void onClick(int pos);
+    }
+
+    private int mNumberOfItems;
+    private String mQueryResults;
+
+    /**
+     * Container for individual movie views
+     * @param numberOfItems Amount of Views to be created.
+     * @param queryResults Json data pulled from server
+     */
+    public MovieAdapter(int numberOfItems, String queryResults, OnItemClickHandler clickHandler) {
+        mNumberOfItems = numberOfItems;
+        mQueryResults = queryResults;
+        mOnClickHandler = clickHandler;
+    }
+
+    @Override
+    public int getItemCount() {
+        return mNumberOfItems;
+    }
+
+    @Override
+    public MovieViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        Context context = parent.getContext();
+
+        //Set the layout of the view
+        int layoutID = R.layout.movie_view;
+        LayoutInflater inflater = LayoutInflater.from(context);
+        boolean attachImmediately = false;
+        View view = inflater.inflate(layoutID, parent, attachImmediately);
+
+        MovieViewHolder viewHolder = new MovieViewHolder(view, context);
+        return viewHolder;
+    }
+
+    @Override
+    public void onBindViewHolder(MovieViewHolder holder, int position) {
+        holder.bind(position);
+    }
+
+    /**
+     * Subclass of RecyclerView.ViewHolder. Helper class to
+     * handle RecyclerView population and recycling.
+     */
+    public class MovieViewHolder extends RecyclerView.ViewHolder implements OnClickListener {
+        //JSONObject keys
+        private static final String TITLE = "title";
+        private static final String POSTER_PATH = "poster_path";
+
+        private TextView mMovieListItemName;
+        private ImageView mMovieListItemImage;
+
+        private Context mContext;
+
+        public MovieViewHolder(View view, Context context) {
+            super(view);
+            mContext = context;
+            mMovieListItemName = (TextView) itemView.findViewById(R.id.tv_movieTitle);
+            mMovieListItemImage = (ImageView) itemView.findViewById(R.id.iv_movieImage);
+            view.setOnClickListener(this);
+        }
+
+        /**
+         * Called by onBindViewHolder to keep things neat. Code to be
+         * executed when a ViewHolder is bound is placed here. Updates
+         * all the views.
+         * @param index Index of the item being bound
+         */
+        public void bind(int index) {
+            String movieName = JsonUtils.getMovieData(mQueryResults, TITLE, index);
+            String posterPath = JsonUtils.getMovieData(mQueryResults, POSTER_PATH, index);
+            String posterUrl = NetworkUtils.buildPosterUrl(posterPath);
+            mMovieListItemName.setText(movieName);
+            Picasso.with(mContext).load(posterUrl).into(mMovieListItemImage);
+        }
+
+        @Override
+        public void onClick(View view) {
+            int adapterPosition = getAdapterPosition();
+            mOnClickHandler.onClick(adapterPosition);
+        }
+    }
+}
